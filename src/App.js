4fe7b5a32b2edc8133e8c588747cd9db660babea
Henry Thach <thachhao@gmail.com>
2019-03-02 08:23:05 -0500

    Add the rest of the app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import pikachu from './pikachu.png'
+import squirtle from './squirtle.png'
+import jigglypuff from './jigglypuff.png'
+import charmander from './charmander.png'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function Pokemon ({ pokemonImage }) {
+  const [reveal, setReveal] = useState(false)
+  let className = 'pokemon'
+
+  if (reveal) {
+    className += ' reveal'
   }
+
+  return (
+    <img
+      src={pokemonImage}
+      className={className}
+      alt='Pokemon'
+      onClick={() => setReveal(!reveal)}
+    />
+  )
 }
 
-export default App;
+const POKEMON_LIST = [
+  pikachu,
+  squirtle,
+  jigglypuff,
+  charmander
+]
+
+function App () {
+  const [index, setIndex] = useState(0)
+
+  return (
+    <div>
+      <div className='App'>
+        <Pokemon
+          key={index}
+          pokemonImage={POKEMON_LIST[index % POKEMON_LIST.length]}
+        />
+      </div>
+      <button
+        className='next-pokemon-button'
+        onClick={() => setIndex(index + 1)}
+      >
+        Next pokemon
+      </button>
+    </div>
+  )
+}
+
+export default App
