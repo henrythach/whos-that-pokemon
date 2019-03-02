ac00e2f46ee29ba033eecf828b52a32523d31028
Henry Thach <thachhao@gmail.com>
2019-03-02 08:30:13 -0500

    Updates

---
+++ precache-manifest.cf6a8f0b9ddc9275fb8a85073818e783.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b446f9ff054cb2de88db84669642d153",
+    "url": "/whos-that-pokemon/static/media/squirtle.b446f9ff.png"
+  },
+  {
+    "revision": "7b59d5e7248b4afd496cdbfa28e212be",
+    "url": "/whos-that-pokemon/static/media/pikachu.7b59d5e7.png"
+  },
+  {
+    "revision": "c9fd5a4f108f9c56d03aba6f28785720",
+    "url": "/whos-that-pokemon/static/media/jigglypuff.c9fd5a4f.png"
+  },
+  {
+    "revision": "bb51e9441d2aac3fbeb513a68dbb49c3",
+    "url": "/whos-that-pokemon/static/media/charmander.bb51e944.png"
+  },
+  {
+    "revision": "58a1af927b56f4e599b81604e1fb16ca",
+    "url": "/whos-that-pokemon/static/media/background.58a1af92.png"
+  },
+  {
+    "revision": "802c3057c7d076d80ce2",
+    "url": "/whos-that-pokemon/static/js/runtime~main.802c3057.js"
+  },
+  {
+    "revision": "0a6c2b1330b1b7545d0d",
+    "url": "/whos-that-pokemon/static/js/main.0a6c2b13.chunk.js"
+  },
+  {
+    "revision": "d05661ab8bfc1fb7cd86",
+    "url": "/whos-that-pokemon/static/js/2.d05661ab.chunk.js"
+  },
+  {
+    "revision": "0a6c2b1330b1b7545d0d",
+    "url": "/whos-that-pokemon/static/css/main.e727babd.chunk.css"
+  },
+  {
+    "revision": "442171bbd4ecf80302bf04a24a6c210e",
+    "url": "/whos-that-pokemon/index.html"
+  }
+];
